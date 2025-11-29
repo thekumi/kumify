d201cdb2f277c304f14fd40aeb3d5ddf6e3aca9c
Kumi <git@kumi.email>
2025-11-29 10:50:36 +0100

    fix: New package name in import

--- kumify/mood/management/commands/fromdaylio.py
+++ kumify/mood/management/commands/fromdaylio.py
@@ -1,27 +1,30 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 
-from mood.models import Status, Mood, Activity, StatusActivity
+from kumify.mood.models import Status, Mood, Activity, StatusActivity
 
 import csv
 from datetime import datetime
 
-DATE_FORMAT = '%Y-%m-%d %I:%M %p'
+DATE_FORMAT = "%Y-%m-%d %I:%M %p"
+
 
 class Command(BaseCommand):
-    help = 'Import .csv from Daylio'
+    help = "Import .csv from Daylio"
 
     def add_arguments(self, parser):
-        parser.add_argument('username')
-        parser.add_argument('path')
+        parser.add_argument("username")
+        parser.add_argument("path")
 
     def handle(self, *args, **options):
         try:
             user = get_user_model().objects.get(username=options["username"])
-            with open(options["path"], mode='r', encoding='utf-8-sig') as infile:
+            with open(options["path"], mode="r", encoding="utf-8-sig") as infile:
                 reader = csv.DictReader(infile)
                 for row in reader:
-                    timestamp = datetime.strptime("%s %s" % (row["full_date"], row["time"]), DATE_FORMAT)
+                    timestamp = datetime.strptime(
+                        "%s %s" % (row["full_date"], row["time"]), DATE_FORMAT
+                    )
                     activities = [a.strip() for a in row["activities"].split("|")]
 
                     try:
@@ -29,12 +32,20 @@ class Command(BaseCommand):
                     except Mood.DoesNotExist:
                         mood = Mood.objects.create(user=user, name=row["mood"], value=0)
 
-                    status = Status.objects.create(user=user, timestamp=timestamp, mood=mood, title=row["note_title"], text=row["note"])
+                    status = Status.objects.create(
+                        user=user,
+                        timestamp=timestamp,
+                        mood=mood,
+                        title=row["note_title"],
+                        text=row["note"],
+                    )
 
                     for activity in activities:
                         if activity:
                             try:
-                                aobj = Activity.objects.get(user=user, name__iexact=activity)
+                                aobj = Activity.objects.get(
+                                    user=user, name__iexact=activity
+                                )
                             except Activity.DoesNotExist:
                                 aobj = Activity.objects.create(user=user, name=activity)
 
@@ -45,4 +56,8 @@ class Command(BaseCommand):
         except get_user_model().DoesNotExist:
             raise CommandError('User "%s" does not exist' % options["user"])
 
-        self.stdout.write(self.style.SUCCESS('Successfully imported data for user "%s"' % options["username"]))
+        self.stdout.write(
+            self.style.SUCCESS(
+                'Successfully imported data for user "%s"' % options["username"]
+            )
+        )
