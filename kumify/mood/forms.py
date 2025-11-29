07c95f70b175b684753d8a2c8495e1e52b9ea46e
Kumi <git@kumi.email>
2025-11-29 11:39:50 +0100

    fix(security): User-scoping mood selection query (low-risk, since it was already being scoped in a later step, but debug output due to a server error in a multi-user installation in debug mode could conceivably expose other users' mood options)

--- kumify/mood/forms.py
+++ kumify/mood/forms.py
@@ -1,4 +1,11 @@
-from django.forms import ModelForm, ModelMultipleChoiceField, Form, CharField, Textarea, BooleanField
+from django.forms import (
+    ModelForm,
+    ModelMultipleChoiceField,
+    BooleanField,
+    Form,
+    CharField,
+    Textarea,
+)
 
 from multiupload.fields import MultiFileField
 
@@ -8,7 +15,8 @@ from .models import Status, Activity
 class StatusForm(ModelForm):
     uploads = MultiFileField(required=False)
     activities = ModelMultipleChoiceField(
-        queryset=Activity.objects.all(), required=False
+        queryset=Activity.objects.none(),  # start with empty; fill in __init__
+        required=False,
     )
     encrypt = BooleanField(required=False)
 
@@ -16,9 +24,15 @@ class StatusForm(ModelForm):
         model = Status
         fields = ["timestamp", "mood", "title", "text"]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, user=None, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.fields["mood"].required = False
+
+        if user is not None:
+            self.fields["activities"].queryset = Activity.objects.filter(user=user)
+        else:
+            self.fields["activities"].queryset = Activity.objects.none()
 
 
 class EncryptorForm(Form):
