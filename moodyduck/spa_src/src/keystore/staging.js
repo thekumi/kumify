8e1d7647590f227f7e99c11e5df3f5f7c34b873c
Kumi <git@kumi.email>
2026-09-20 21:25:47 +0200

    Revert "debug: Add debug output"
    This reverts commit a05019645965e645a984dc42823dcbd6293b8c44.

--- moodyduck/spa_src/src/keystore/staging.js
+++ moodyduck/spa_src/src/keystore/staging.js
@@ -64,7 +64,6 @@ export async function runStaging(dataKey) {
       + mediaItems.length
       + (profileUpgrade ? 1 : 0)
       + (medicalInfoUpgrade ? 1 : 0)
-    console.debug('[staging] fetched', { totalFetched, statusUpgrades: statusUpgrades.length })
     if (totalFetched === 0) break
 
     const patch = {}
@@ -193,7 +192,6 @@ export async function runStaging(dataKey) {
       }
     }
 
-    console.debug('[staging] batchCount', batchCount, 'patch keys', Object.keys(patch).map(k => `${k}:${patch[k]?.length ?? '?'}`))
     if (batchCount === 0 && mediaCount === 0) break
 
     let stagingUpdated = 0
